38baa261549e7acdadf57afdad629d006020155d
Flappeh <bonar_david@rocketmail.com>
2024-06-07 13:38:06 +0700

    Initial upload

---
+++ README.md
@@ -0,0 +1,3 @@
+#My Own Portfolio
+
+This is my take on my own portfolio using React, Parallax, 3djs
---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+
+
+export default function Home() {
+  return (
+    <div>
+        <Parallax pages={4}>
+            <ParallaxLayer>
+                <h2>
+                    asf
+                    </h2>
+            </ParallaxLayer>
+            <ParallaxLayer>
+                <h2>
+                    sdfasdf
+                    </h2>
+            </ParallaxLayer>
+        </Parallax>
+
+    </div>
+  )
+}
